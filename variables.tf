764aec1d27ce28112b80c4ee0c6cbec28d9d969e
mehdijebali <mehdi.benhedi.jebali@gmail.com>
2022-09-13 14:20:25 +0100

    Create variables for autoscaling resource

--- variables.tf
+++ variables.tf
@@ -17,3 +17,64 @@ variable "PATH_TO_PUBLIC_KEY" {
 variable "INSTANCE_TYPE" {
   default = "t2.micro"
 }
+
+variable "VPC_ZONE_IDENTIFIER" {
+  type = list
+  default = ["subnet-0855e6a23018a26b9","subnet-0cc3c7e14fbd1d829"]
+}
+
+variable "LAUNCH_CONFIGURATION_NAME" {
+  default = "tf-launchconfig"
+}
+
+variable "KEY_NAME" {
+  default = "tf-key"
+}
+
+variable "AUTOSCALING_GROUP_NAME" {
+  default = "tf-autoscaling"
+}
+
+variable "AUTOSCALING_GROUP_MIN_SIZE" {
+  default = 1
+}
+
+variable "AUTOSCALING_GROUP_MAX_SIZE" {
+  default = 2
+}
+
+variable "HEALTH_CHECK_GRACE_PERIOD" {
+  default = 200
+}
+
+variable "HEALTH_CHECK_TYPE" {
+  default = "EC2"
+}
+
+variable "AUTOSCALING_POLICY_NAME" {
+  default = "tf-cpu-policy"
+}
+
+variable "AUTO_DE_SCALING_POLICY_NAME" {
+  default = "tf-cpu-policy-scaledown"
+}
+
+variable "AUTOSCALING_POLICY_ADJUSTMENT_TYPE" {
+  default = "ChangeInCapacity"
+}
+
+variable "AUTOSCALING_POLICY_TYPE" {
+  default = "SimpleScaling"
+}
+
+variable "AUTOSCALING_POLICY_SCALING_ADJUSTMENT" {
+  default = "1"
+}
+
+variable "AUTO_DE_SCALING_POLICY_SCALING_ADJUSTMENT" {
+  default = "-1"
+}
+
+variable "AUTOSCALING_POLICY_COOLDOWN" {
+  default = "200"
+}

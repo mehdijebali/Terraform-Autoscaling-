45ec5127439ed82ab87e364dd96f5aacf16f3eea
mehdijebali <mehdi.benhedi.jebali@gmail.com>
2023-01-05 16:53:53 +0100

    Configure launch configuration resource with custom AMI

--- provider.tf
+++ provider.tf
@@ -1,3 +1,13 @@
 provider "aws" {
   region     = var.AWS_REGION
 }
+
+data "aws_ami" "packer_ami" {
+  most_recent      = true
+  owners           = ["self"]
+
+  filter {
+    name   = "name"
+    values = ["custom-configuration-*"]
+  }
+}

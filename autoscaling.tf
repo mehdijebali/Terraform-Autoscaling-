b382ff55ad5a57b24bd0dbc7ef0af915598a5793
mehdijebali <mehdi.benhedi.jebali@gmail.com>
2023-01-13 11:07:33 +0100

    Configure autoscaling greoup with launch template

--- autoscaling.tf
+++ autoscaling.tf
@@ -1,12 +1,4 @@
-#AutoScaling Launch Configuration
-resource "aws_launch_configuration" "tf-launchconfig" {
-  name_prefix     = var.LAUNCH_CONFIGURATION_NAME
-  image_id        = data.aws_ami.packer_ami.id
-  instance_type   = var.INSTANCE_TYPE
-  key_name        = aws_key_pair.tf-ssh-key.key_name
-  security_groups = [aws_security_group.allow-levelup-ssh.id]
-}
-
+#AutoScaling Launch Template
 resource "aws_launch_template" "tf-launchtemplate" {
   name = var.LAUNCH_TEMPLATE_NAME
   image_id = data.aws_ami.packer_ami.id
@@ -25,13 +17,14 @@ resource "aws_key_pair" "tf-ssh-key" {
 resource "aws_autoscaling_group" "tf-autoscaling" {
   name                      = var.AUTOSCALING_GROUP_NAME
   vpc_zone_identifier       = var.VPC_ZONE_IDENTIFIER
-  launch_configuration      = aws_launch_configuration.tf-launchconfig.name
   min_size                  = var.AUTOSCALING_GROUP_MIN_SIZE
   max_size                  = var.AUTOSCALING_GROUP_MAX_SIZE
   health_check_grace_period = var.HEALTH_CHECK_GRACE_PERIOD
   health_check_type         = var.HEALTH_CHECK_TYPE
   force_delete              = true
-
+  launch_template {
+    id = aws_launch_template.tf-launchtemplate.id
+  }
   tag {
     key                 = "Name"
     value               = "LevelUp Custom EC2 instance"

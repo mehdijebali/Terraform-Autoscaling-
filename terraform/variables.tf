70e831f9627e6b8d899d6eda787478a9613440a5
mehdijebali <mehdi.benhedi.jebali@gmail.com>
2025-09-13 21:21:53 +0200

    ✨ Feat/Add vars descriptions

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,99 +1,119 @@
 variable "AWS_REGION" {
-  type    = string
-  default = "us-east-1"
+  type        = string
+  description = "The region where resources are deployed"
+  default     = "us-east-1"
 }
 
 variable "PATH_TO_PUBLIC_KEY" {
-  type    = string
-  default = "levelup_key.pub"
+  type        = string
+  description = "Path to SSH public key"
+  default     = "levelup_key.pub"
 }
 
 variable "INSTANCE_TYPE" {
-  type    = string
-  default = "t2.micro"
+  type        = string
+  description = "AWS Instance type"
+  default     = "t2.micro"
 }
 
 variable "VPC_ZONE_IDENTIFIER" {
-  type    = list(any)
-  default = ["subnet-0e5871e683b310bc3", "subnet-04b04399210978637"]
+  type        = list(any)
+  description = "List of VPC Subnets"
+  default     = ["subnet-0e5871e683b310bc3", "subnet-04b04399210978637"]
 }
 
 variable "LAUNCH_TEMPLATE_NAME" {
-  type    = string
-  default = "tf-launchtemplate"
+  type        = string
+  description = "Launch Template Name"
+  default     = "tf-launchtemplate"
 }
 
 variable "KEY_NAME" {
-  type    = string
-  default = "tf-ssh-key"
+  type        = string
+  description = "SSH key name"
+  default     = "tf-ssh-key"
 }
 
 variable "AUTOSCALING_GROUP_NAME" {
-  type    = string
-  default = "tf-autoscaling"
+  type        = string
+  description = "Auto Scaling Group name"
+  default     = "tf-autoscaling"
 }
 
 variable "AUTOSCALING_GROUP_MIN_SIZE" {
-  type    = number
-  default = 1
+  type        = number
+  description = "Auto-Scaling Minimum EC2 Instances number"
+  default     = 1
 }
 
 variable "AUTOSCALING_GROUP_MAX_SIZE" {
-  type    = number
-  default = 2
+  type        = number
+  description = "Auto-Scaling Maximum EC2 Instances number"
+  default     = 2
 }
 
 variable "HEALTH_CHECK_GRACE_PERIOD" {
-  type    = number
-  default = 200
+  type        = number
+  description = "Health Check Period in seconds"
+  default     = 200
 }
 
 variable "HEALTH_CHECK_TYPE" {
-  type    = string
-  default = "EC2"
+  type        = string
+  description = "Health Check Type"
+  default     = "EC2"
 }
 
 variable "AUTOSCALING_POLICY_NAME" {
-  type    = string
-  default = "tf-cpu-policy"
+  type        = string
+  description = "Auto-Scaling Policy Name"
+  default     = "tf-cpu-policy"
 }
 
 variable "AUTO_DE_SCALING_POLICY_NAME" {
-  type    = string
-  default = "tf-cpu-policy-scaledown"
+  type        = string
+  description = "Auto-DEScaling Policy Name"
+  default     = "tf-cpu-policy-scaledown"
 }
 
 variable "AUTOSCALING_POLICY_ADJUSTMENT_TYPE" {
-  type    = string
-  default = "ChangeInCapacity"
+  type        = string
+  description = "Auto-Scaling Policy Adjustment Type"
+  default     = "ChangeInCapacity"
 }
 
 variable "AUTOSCALING_POLICY_TYPE" {
-  type    = string
-  default = "SimpleScaling"
+  type        = string
+  description = "Auto-Scaling Policy Type"
+  default     = "SimpleScaling"
 }
 
 variable "AUTOSCALING_POLICY_SCALING_ADJUSTMENT" {
-  type    = string
-  default = "1"
+  type        = string
+  description = "Auto-Scaling Adjustment Policy"
+  default     = "1"
 }
 
 variable "AUTO_DE_SCALING_POLICY_SCALING_ADJUSTMENT" {
-  type    = string
-  default = "-1"
+  type        = string
+  description = "Auto-DEScaling Adjustment Policy"
+  default     = "-1"
 }
 
 variable "AUTOSCALING_POLICY_COOLDOWN" {
-  type    = string
-  default = "200"
+  type        = string
+  description = "Auto-Scaling CoolDown Policy"
+  default     = "200"
 }
 
 variable "SG_DESCRIPTION" {
-  type    = string
-  default = "security group that allows ssh connection"
+  type        = string
+  description = "EC2 Security Group Description"
+  default     = "security group that allows ssh connection"
 }
 
 variable "LD_NAME" {
-  type    = string
-  default = "centos"
+  type        = string
+  description = "Linux Distribution Name"
+  default     = "centos"
 }

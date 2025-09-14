694b40bb42edf3641e54233141df39344c2deeac
mehdijebali <mehdi.benhedi.jebali@gmail.com>
2025-09-14 12:31:29 +0200

    ✨ Feat/Add default tags

--- terraform/locals.tf
+++ terraform/locals.tf
@@ -1,8 +1,11 @@
 locals {
   SG_NAME  = "allow-levelup-ssh"
   ami-name = var.LD_NAME == "centos" ? "centos-template-pkr-*" : "ubuntu-template-pkr-*"
-
   sg-tag = {
     Name = local.SG_NAME
+  }
+  default-tags = {
+    Stack       = "Auto-Scaling",
+    Environment = "Production"
   }
 }
